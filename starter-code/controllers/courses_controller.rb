bd2b422caf3ac769cec93b101e68371664896711
adaau <ada.au.cheng@gmail.com>
2015-10-26 23:41:32 +0800

    added controllers and views for courses

--- starter-code/controllers/courses_controller.rb
+++ starter-code/controllers/courses_controller.rb
@@ -1,30 +1,56 @@
 class RecipeApp < Sinatra::Base
   # index
-
-
+  get "/courses" do
+    @courses = Course.all
+    erb(:"courses/index")
+  end
 
   # new
-
-
+  get "/courses/new" do
+    @course = Course.new
+    erb(:"courses/new")
+  end
 
   # create
-
-
+  post "/courses" do
+    @course = Course.new(params[:course])
+    if @course.save
+      redirect("/courses/#{@course.id}")
+    else
+      erb(:"courses/new")
+    end
+  end
 
   # show
-
-
+  get "/courses/:id" do
+    @course = Course.find(params[:id])
+    erb(:"courses/show")
+  end
 
   # edit
-
-
+  get "/courses/:id/edit" do
+    @course = Course.find(params[:id])
+    erb(:"courses/edit")
+  end
 
   # update
-
-
+  post "/courses/:id/update" do
+    @course = Course.find(params[:id])
+    if @course.update(params[:course])
+      redirect("/courses")
+    else
+      erb(:"courses/new")
+    end
+  end
 
   # delete
-
-
+  post "/courses/:id/delete" do
+    @course = Course.find(params[:id])
+    if @course.destroy
+      redirect("/courses")
+    else
+      redirect("/courses/#{@course.id}")
+    end
+  end
 
 end
