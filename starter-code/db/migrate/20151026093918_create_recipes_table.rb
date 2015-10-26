fc7314dba0fdba3d607634f81de082f3f6ea04b9
adaau <ada.au.cheng@gmail.com>
2015-10-26 20:28:49 +0800

    done

---
+++ starter-code/db/migrate/20151026093918_create_recipes_table.rb
@@ -0,0 +1,11 @@
+class CreateRecipesTable < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.text :description
+      t.integer :servings
+
+      t.timestamps
+    end
+  end
+end
